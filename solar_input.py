70a62332ec254d121fb438bc8385ac362b455bfb
BazhenovaSvetlana <bazhenovasvetlana02@gmail.ru>
2020-11-08 09:21:48 +0300

    вроде правильно написала две строчки

--- solar_input.py
+++ solar_input.py
@@ -94,8 +94,8 @@ def write_space_objects_data_to_file(output_filename, space_objects):
         for obj in space_objects:
             print(out_file, "%s %d %s %f" % ('1', 2, '3', 4.5))
             # FIXME: should store real values
-            planet_param = str(planet.R)+planet.color+str(planet.m)+str(planet.x)+str(planet.y)+str(planet.Vx)+str(planet.Vy)
-            star_param = str(star.R) + star.color + str(star.m) + str(star.x) + str(star.y) + str(star.Vx) + str(star.Vy)
+            planet_param = str(planet.R) + ' ' + planet.color + ' ' + str(planet.m) + ' ' + str(planet.x) + ' ' + str(planet.y) + ' ' + str(planet.Vx) + ' ' + str(planet.Vy)
+            star_param = str(star.R) + ' ' + star.color + ' ' + str(star.m) + ' ' + str(star.x) + ' ' + str(star.y) + ' ' + str(star.Vx) + ' ' + str(star.Vy)
 # FIXME: хорошо бы ещё сделать функцию, сохранающую статистику в заданный файл...
 
 if __name__ == "__main__":
